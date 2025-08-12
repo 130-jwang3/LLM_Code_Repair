72989c051cdb70d2b3f309b16600259b2f3423d4
Jonny <J_lin87@hotmail.com>
2025-08-12 15:59:50 -0700

    added splitting of graph

--- src/input_splitter.py
+++ src/input_splitter.py
@@ -1,6 +1,7 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveJsonSplitter
+import tiktoken
 import json
-import os
 
 # returns list of dictionaries, first one being file tree, the rest will be file sections and its contents
 def split_text(input, chunk_size):
@@ -33,14 +34,56 @@ def split_text(input, chunk_size):
         return separated_text
 
 def split_ast(input, chunk_size):
+    modules = {}
+    results = []
+    try: 
+        with open(input) as f:
+            src = json.load(f)['nodes']
+
+        #first group up the nodes by module
+        for node in src:
+            module = node['module']
+            if module not in modules:
+                modules[module] = []
+            modules[module].append(node)
+
+        #will treat the group of nodes as text and split by the border of the nodes 
+        splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder (
+            encoding_name='cl100k_base',
+            chunk_size=chunk_size,
+            chunk_overlap = 50,
+            separators= ["}, {"]
+        )
+
+        #if module is too big, it will be split with labeled sections 
+        for k,v in modules.items():
+            chunks = splitter.split_text(json.dumps(v))
+            for j, sec in enumerate(chunks):
+                results.append({"module": k, "section" : j+1, "nodes": sec})
+        print(len(results))             
+
+    except FileNotFoundError:
+        print(f"Error: {input} not found")
+    except json.JSONDecodeError:
+        print("Error: Invalid JSON ")
     
-    pass
+
+    return results
 
 # example usage of split_text
 # def main():
 
+#     nodes = split_ast("data/graphs/graph.json", 4000)
+#     print(nodes[49])
+#     print('\n')
+#     print(nodes[50])
+#     print('\n')
+#     print(nodes[51])
+
+
 #     separated = split_text("LLM_Code_Repair/data/text/repo_text_bundle.json", 4000)
 #     print(len(separated))
 #     print(separated[99])
+
 
 # main()
